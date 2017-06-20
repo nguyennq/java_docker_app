841577618902ec6d09a5de59f902a0f92df540d0
nals <nguyen_nq@hotmail.com>
2017-06-20 13:20:25 +0700

    build app

---
+++ helloworld/src/main/java/vn/nguyen/App.java
@@ -0,0 +1,13 @@
+package vn.nguyen;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
